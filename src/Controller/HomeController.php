6a7083e984540e1391046f8480c063d2cd050319
--user.email <hacenedu92@live.fr>
2023-01-06 19:07:36 +0100

    now I have just designed one form per figure, in my maudale.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,10 +10,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $formFactory;
+
+    public function __construct(FormFactoryInterface $formFactory)
+    {
+        $this->formFactory = $formFactory;
+    }
     const ITEMS_PER_PAGE = 6;
     #[Route('/home/figure/page/{page}', name: 'app_home')]
     public function index(Request $request, FigureRepository $fgr, int $page = 1): Response
@@ -22,16 +29,19 @@ class HomeController extends AbstractController
         $totalPages = ceil($totalItems / self::ITEMS_PER_PAGE);
         $offset = ($page - 1) * self::ITEMS_PER_PAGE;
         $figures = $fgr->findBy([], [], self::ITEMS_PER_PAGE, $offset);
-
-        $comment = new Comments();
-        $form = $this->createForm(CommentsType::class, $comment);
-         $form->handleRequest($request);
-
+        $forms = [];
+        foreach ($figures as $figure) {
+            $comment = new Comments();
+            $form = $this->formFactory->create(CommentsType::class, $comment);
+            $form->handleRequest($request);
+            $forms[$figure->getId()] = $form->createView();
+            unset($form);
+        }
         return $this->renderForm('home/index.html.twig', [
             'figures' => $figures,
             'current_page' => $page,
             'total_pages' => $totalPages,
-            'form' => $form,
+            'forms' => $forms,
         ]);
     }
 
