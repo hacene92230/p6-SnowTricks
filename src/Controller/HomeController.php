7321d6a3f58acc4f2e226776fd41d598d9c63913
--user.email <hacenedu92@live.fr>
2023-01-07 19:56:37 +0100

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,10 +21,9 @@ class HomeController extends AbstractController
     {
         $this->formFactory = $formFactory;
     }
-    const FIGURES_PER_PAGE = 6;
-    const COMMENTS_PER_PAGE = 3;
-
-    #[Route('/home/figure/page/{page}', name: 'app_home')]
+    const FIGURES_PER_PAGE = 5;
+    
+    #[Route('/homes/page/{page}', name: 'app_home')]
     public function index(Request $request, FigureRepository $fgr, int $page = 1): Response
     {
         // Récupération du nombre total d'éléments à paginer
@@ -39,22 +38,9 @@ class HomeController extends AbstractController
         // Récupération des figures de la page courante à partir de l'offset et du nombre d'éléments par page
         $figures = $fgr->findBy([], [], self::FIGURES_PER_PAGE, $offset);
 
-        // Initialisation de la variable commentsOffset avant la boucle foreach
-        $commentsOffset = 0;
-
         // Création des formulaires de commentaire pour chaque figure
         $forms = [];
         foreach ($figures as $figure) {
-
-            // Récupération des commentaires de la page courante pour chaque figure
-            $comments = $figure->getComments()->toArray();
-            $totalComments = count($comments);
-            $commentsPagesCount = ceil($totalComments / self::COMMENTS_PER_PAGE);
-            // Mise à jour de la valeur de commentsOffset à chaque itération de la boucle
-            $commentsOffset = ($page - 1) * self::COMMENTS_PER_PAGE;
-
-            $figureComments = array_slice($comments, $commentsOffset, self::COMMENTS_PER_PAGE);
-
             $comment = new Comments();
             $form = $this->formFactory->create(CommentsType::class, $comment);
             $form->handleRequest($request);
@@ -62,19 +48,14 @@ class HomeController extends AbstractController
             unset($form);
         }
 
-
         // Affichage de la vue avec les données de pagination
         return $this->renderForm('home/index.html.twig', [
             'figures' => $figures,
             'current_page' => $page,
             'figurePagesCount' => $totalPages,
-            'figure_comments' => $figureComments,
-            'commentsPagesCount' => $commentsPagesCount,
             'forms' => $forms,
-            'commentsOffset' => $commentsOffset,
             'page' => $page,
-            'COMMENTS_PER_PAGE' => self::COMMENTS_PER_PAGE,
-            'totalComments' => $totalComments,
+
             'total_pages' => $totalPages
         ]);
     }
