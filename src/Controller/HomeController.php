bb846942c08e38f7c2395e8fa469134b20a741e9
--user.email <hacenedu92@live.fr>
2023-01-07 13:16:28 +0100

    test a paginate comments

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,29 +21,64 @@ class HomeController extends AbstractController
     {
         $this->formFactory = $formFactory;
     }
-    const ITEMS_PER_PAGE = 6;
+    const FIGURES_PER_PAGE = 6;
+    const COMMENTS_PER_PAGE = 3;
+
     #[Route('/home/figure/page/{page}', name: 'app_home')]
     public function index(Request $request, FigureRepository $fgr, int $page = 1): Response
     {
+        // Récupération du nombre total d'éléments à paginer
         $totalItems = $fgr->count([]);
-        $totalPages = ceil($totalItems / self::ITEMS_PER_PAGE);
-        $offset = ($page - 1) * self::ITEMS_PER_PAGE;
-        $figures = $fgr->findBy([], [], self::ITEMS_PER_PAGE, $offset);
+
+        // Calcul du nombre total de pages en divisant le nombre total d'éléments par le nombre d'éléments par page
+        $totalPages = ceil($totalItems / self::FIGURES_PER_PAGE);
+
+        // Calcul de l'offset à partir du numéro de la page courante et du nombre d'éléments par page
+        $offset = ($page - 1) * self::FIGURES_PER_PAGE;
+
+        // Récupération des figures de la page courante à partir de l'offset et du nombre d'éléments par page
+        $figures = $fgr->findBy([], [], self::FIGURES_PER_PAGE, $offset);
+
+        // Initialisation de la variable commentsOffset avant la boucle foreach
+        $commentsOffset = 0;
+
+        // Création des formulaires de commentaire pour chaque figure
         $forms = [];
         foreach ($figures as $figure) {
+
+            // Récupération des commentaires de la page courante pour chaque figure
+            $comments = $figure->getComments()->toArray();
+            $totalComments = count($comments);
+            $commentsPagesCount = ceil($totalComments / self::COMMENTS_PER_PAGE);
+            // Mise à jour de la valeur de commentsOffset à chaque itération de la boucle
+            $commentsOffset = ($page - 1) * self::COMMENTS_PER_PAGE;
+
+            $figureComments = array_slice($comments, $commentsOffset, self::COMMENTS_PER_PAGE);
+
             $comment = new Comments();
             $form = $this->formFactory->create(CommentsType::class, $comment);
             $form->handleRequest($request);
             $forms[$figure->getId()] = $form->createView();
             unset($form);
         }
+
+
+        // Affichage de la vue avec les données de pagination
         return $this->renderForm('home/index.html.twig', [
             'figures' => $figures,
             'current_page' => $page,
-            'total_pages' => $totalPages,
+            'figurePagesCount' => $totalPages,
+            'figure_comments' => $figureComments,
+            'commentsPagesCount' => $commentsPagesCount,
             'forms' => $forms,
+            'commentsOffset' => $commentsOffset,
+            'page' => $page,
+            'COMMENTS_PER_PAGE' => self::COMMENTS_PER_PAGE,
+            'totalComments' => $totalComments,
+            'total_pages' => $totalPages
         ]);
     }
+
 
 
 
