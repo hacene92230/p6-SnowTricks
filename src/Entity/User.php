53e08f26afd4385174b45359831f90e1c418b44e
--user.email <hacenedu92@live.fr>
2023-02-09 10:30:01 +0100

    main

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $avatar = null;
+    private ?string $avatar;
 
     public function __construct()
     {
